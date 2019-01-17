56faaa5af2a8e1244f6fbab1ab7e60015cf1772f
Fgshop <itsddan@gmail.com>
2019-01-17 15:29:31 +0900

    edit make

--- coin_generator.sh
+++ coin_generator.sh
@@ -327,7 +327,7 @@ build_new_coin()
         bash ./configure --disable-tests --disable-bench
     fi
     # always build as the user could have manually changed some files
-    make -f makefile.unix USE_UPNP=-
+    make -f makefile.am USE_UPNP=-
 }
 
 
